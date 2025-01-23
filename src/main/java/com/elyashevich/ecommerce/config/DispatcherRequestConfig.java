491661d4eab35b5c52d09f3cbfa48a1202e5b088
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2025-01-23 22:00:42 +0300

    Add: mvc config

---
+++ src/main/java/com/elyashevich/ecommerce/config/DispatcherRequestConfig.java
@@ -0,0 +1,21 @@
+package com.elyashevich.ecommerce.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class DispatcherRequestConfig implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        var context = new AnnotationConfigWebApplicationContext();
+        context.register(MvcConfig.class);
+
+        var servlet = new DispatcherServlet(context);
+        var registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/app/*");
+    }
+}
---
+++ src/main/java/com/elyashevich/ecommerce/config/MvcConfig.java
@@ -0,0 +1,47 @@
+package com.elyashevich.ecommerce.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.elyashevich.ecommerce")
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        var templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        var templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(this.templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        var resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(this.templateEngine());
+        registry.viewResolver(resolver);
+    }
+}
