153561040a2490ecfb4c86a887cc2ff520f93f65
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2025-02-03 15:27:22 +0300

    Add: exception controller advice

---
+++ src/main/java/com/elyashevich/ecommerce/controller/ExceptionHandlerController.java
@@ -0,0 +1,19 @@
+package com.elyashevich.ecommerce.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error/error");
+        return mav;
+    }
+}
